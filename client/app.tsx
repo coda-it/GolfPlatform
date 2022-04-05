f35cd6ed0102585a3fe6477fa6dd1551ea14d424
Oskar Szura <oskar.szura@gmail.com>
2022-04-05 09:37:44 +0200

    CODA-249 - Associate SQL Server with AppService (#9)

--- client/app.tsx
+++ client/app.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { Router } from 'react-router';
-import { createBrowserHistory } from 'history';
-import createSagaMiddleware from 'redux-saga';
-import sagas from './sagas';
-import reducers from './reducers';
+import React from "react";
+import { render } from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { Router } from "react-router";
+import { createBrowserHistory } from "history";
+import createSagaMiddleware from "redux-saga";
+import sagas from "./sagas";
+import reducers from "./reducers";
 
-const appContainer = document.querySelector('.js-app');
+const appContainer = document.querySelector(".js-app");
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(reducers, applyMiddleware(sagaMiddleware));
